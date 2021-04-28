8fb2ac7aafac480b83300e8ad015451a8d2d1df9
Leandro Ricardo Wormsbecher <leandro.wormsbecher@agrotis.com>
2021-04-27 21:04:26 -0300

    Adicionado docker

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@ const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: '0.0.0.0'
     });
 
     server.route({
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-
             return 'Hello World!';
         }
-    });    
+    });
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
