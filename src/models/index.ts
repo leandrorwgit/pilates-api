b246f47a31bc3a23ce858cdf27ba80c59a9f5827
Leandro Ricardo Wormsbecher <leandro.wormsbecher@agrotis.com>
2021-05-03 22:04:09 -0300

    Correção config banco.

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,6 +6,12 @@ dotenv.config();
 const  sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres',
 	protocol: 'postgres',
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false
+		}
+	}	
 });
 
 export { Sequelize, sequelize };
