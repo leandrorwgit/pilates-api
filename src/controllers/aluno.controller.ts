6904f55c4c98e6e4b816af0d739229d66344912e
Leandro Ricardo Wormsbecher <leandro.wormsbecher@agrotis.com>
2021-05-05 22:32:57 -0300

    Alterações listar alunos

--- src/controllers/aluno.controller.ts
+++ src/controllers/aluno.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { Validadoes } from '../util/validacoes-comuns';
 import { Aluno } from '../models/aluno';
 import { getMensagemErro, InternalServerError } from '../util/erros';
+import { Op } from 'sequelize';
 
 export class AlunoController {
 
@@ -33,7 +34,7 @@ export class AlunoController {
 
       res.status(200).send(aluno);
     } catch (erro) {
-      res.status(400).send({ mensagem: getMensagemErro(erro) });
+      res.status(400).send({ mensagem: 'Erro ao inserir aluno: '+getMensagemErro(erro) });
     }
   }
 
@@ -71,48 +72,51 @@ export class AlunoController {
 
       res.status(200).send(alunoAtualizado);
     } catch (erro) {
-      res.status(400).send({ mensagem: getMensagemErro(erro) });
+      res.status(400).send({ mensagem: 'Erro ao atualizar aluno: '+getMensagemErro(erro) });
     }
   }
 
   public async deletar(req: Request, res: Response) {
     try {
-      /* 
-      const produto = await Produto.findByPk(1);
-produto.destroy();
-*/
       Validadoes.campoStringNaoNulo(req.params.id, 'id');
-      Validadoes.campoStringNaoNulo(req.body.nome, 'nome');
+      Validadoes.campoStringNaoNulo(req['usuario'].id, 'idUsuario');
 
       const aluno = await Aluno.findByPk(req.params.id);
-      aluno.nome = req.body.nome;
-      aluno.idade = req.body.idade;
-      aluno.dataNascimento = req.body.dataNascimento;
-      aluno.profissao = req.body.profissao;
-      aluno.celular = req.body.celular;
-      aluno.email = req.body.email;
-      aluno.objetivosPilates = req.body.objetivosPilates;
-      aluno.queixas = req.body.queixas;
-      aluno.formaPagamento = req.body.formaPagamento;
-      aluno.diaPagamento = req.body.diaPagamento;
-      aluno.aulaSeg = req.body.aulaSeg;
-      aluno.aulaTer = req.body.aulaTer;
-      aluno.aulaQua = req.body.aulaQua;
-      aluno.aulaQui = req.body.aulaQui;
-      aluno.aulaSex = req.body.aulaSex;
-      aluno.aulaSab = req.body.aulaSab;
-      aluno.ativo = req.body.ativo;
-      const alunoAtualizado = await aluno.save();
 
-      res.status(200).send(alunoAtualizado);
+      if (aluno.idUsuario != req['usuario'].id) {
+        throw new InternalServerError(`Usuário ${req['usuario'].id} não pode excluir esse registro.`);
+      }
+ 
+      aluno.destroy();
+
+      res.status(204).send();
     } catch (erro) {
-      res.status(400).send({ mensagem: getMensagemErro(erro) });
+      res.status(400).send({ mensagem: 'Erro ao deletar aluno: '+getMensagemErro(erro) });
     }
   }  
 
   public async listar(req: Request, res: Response) {
     //const alunos = await Aluno.findAll({ include: ["usuario"] });
-    const alunos = await Aluno.findAll();
+    Validadoes.campoStringNaoNulo(req['usuario'].id, 'idUsuario');
+
+    const whereNome = req.query.nome ? {nome: {[Op.iLike]: req.query.nome}} : {};
+    const whereAtivo = req.query.ativo ? {ativo: req.query.ativo} : {};
+    
+    const offset = req.query.pagina && req.query.tamanhoMax ? 
+      (Number.parseInt(req.query.pagina.toString()) * Number.parseInt(req.query.tamanhoMax.toString())) : null;
+    const limit = req.query.tamanhoMax ? Number.parseInt(req.query.tamanhoMax.toString()) : null;
+
+    const alunos = await Aluno.findAll({ 
+      where: {
+        [Op.and]: [
+          {idUsuario: req['usuario'].id},
+          whereNome,
+          whereAtivo
+        ]
+      },
+      offset: offset,
+      limit: limit,
+    });
     res.send(alunos);
   }  
 }
