93f738857bd72ad77da3bf628f1cc99a1d83ff63
Leandro Ricardo Wormsbecher <leandro.wormsbecher@agrotis.com>
2021-05-27 00:00:35 -0300

    Alterado SQL para recuperar dados para agenda.

--- src/controllers/agenda.controller.ts
+++ src/controllers/agenda.controller.ts
@@ -11,18 +11,21 @@ export class AgendaController {
     
     const retorno = await sequelize.query(
       'select '+
-      'aluno.nome as descricao, '+
+      'aluno."nome" as descricao, '+
       'cast(aluno."aulaHorarioInicio" as time) as horaIni, '+
       'cast(aluno."aulaHorarioInicio" as time) + (aluno."aulaDuracao"||\' min\')::interval as horaFim, '+
-      'date :data as dia '+
+      'dias.dia as dia '+
       'from "Aluno" as aluno '+
+      'join (SELECT * FROM (VALUES (date :data - 1), (date :data), (date :data + 1)) AS d (dia)) as dias on true '+
       'where aluno."idUsuario" = :idUsuario '+
-      'and (((extract(isodow from date :data) = 1) and (aluno."aulaSeg" = true)) '+
-      'or ((extract(isodow from date :data) = 2) and (aluno."aulaTer" = true)) '+
-      'or ((extract(isodow from date :data) = 3) and (aluno."aulaQua" = true)) '+
-      'or ((extract(isodow from date :data) = 4) and (aluno."aulaQui" = true)) '+
-      'or ((extract(isodow from date :data) = 5) and (aluno."aulaSex" = true)) '+
-      'or ((extract(isodow from date :data) = 6) and (aluno."aulaSab" = true))) ',
+      'and aluno."ativo" = true '+
+      'and (((extract(isodow from dias.dia) = 1) and (aluno."aulaSeg" = true)) '+
+      'or ((extract(isodow from dias.dia) = 2) and (aluno."aulaTer" = true)) '+
+      'or ((extract(isodow from dias.dia) = 3) and (aluno."aulaQua" = true)) '+
+      'or ((extract(isodow from dias.dia) = 4) and (aluno."aulaQui" = true)) '+
+      'or ((extract(isodow from dias.dia) = 5) and (aluno."aulaSex" = true)) '+
+      'or ((extract(isodow from dias.dia) = 6) and (aluno."aulaSab" = true))) '+
+      'order by dias.dia, cast(aluno."aulaHorarioInicio" as time) ',
       {
         replacements: { idUsuario: req['usuario'].id, data: req.query.data },
         type: QueryTypes.SELECT
